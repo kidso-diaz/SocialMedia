d1fec7bb87d6d69e8f415de7a34f497daa6e9a29
gubiarpa <billy.arredondo@pucp.pe>
2021-07-07 20:16:34 -0500

    dummy flow

---
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPosts()
+        {
+            var posts = new PostRepository().GetPosts();
+            return Ok(posts);
+        }
+    }
+}
