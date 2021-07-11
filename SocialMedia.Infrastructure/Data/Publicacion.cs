4f3c33e504048e2252eca6b7a8e513670268c7e0
gubiarpa <billy.arredondo@pucp.pe>
2021-07-11 12:53:07 -0500

    creating model in db contect
    Scaffold-DbContext "Server={{ server }};Database={{ database }};User Id={{ user }};password={{ password }}" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Data

---
+++ SocialMedia.Infrastructure/Data/Publicacion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace SocialMedia.Infrastructure.Data
+{
+    public partial class Publicacion
+    {
+        public Publicacion()
+        {
+            Comentario = new HashSet<Comentario>();
+        }
+
+        public int IdPublicacion { get; set; }
+        public int IdUsuario { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Descripcion { get; set; }
+        public string Imagen { get; set; }
+
+        public virtual Usuario IdUsuarioNavigation { get; set; }
+        public virtual ICollection<Comentario> Comentario { get; set; }
+    }
+}
