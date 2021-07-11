def5dd452834bdfad17ebebd9d9204d81d399c3e
gubiarpa <billy.arredondo@pucp.pe>
2021-07-10 22:29:10 -0500

    post mongo example

--- SocialMedia.Infrastructure/Repositories/PostMongoRepository.cs
+++ SocialMedia.Infrastructure/Repositories/PostMongoRepository.cs
@@ -2,14 +2,14 @@
 using SocialMedia.Core.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SocialMedia.Infrastructure.Repositories
 {
     public class PostMongoRepository : IPostRepository
     {
-        public Task<IEnumerable<Post>> GetPosts()
+        public async Task<IEnumerable<Post>> GetPosts()
         {
             var posts = Enumerable.Range(1, 10).Select(x => new Post()
             {
