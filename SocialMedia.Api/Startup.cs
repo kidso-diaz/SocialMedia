825278a30148e0eea577bb50f12ab8fcbdb8b368
gubiarpa <billy.arredondo@pucp.pe>
2021-07-17 18:39:47 -0500

    newtosoft added
    Microsoft.AspNetCore.Mvc.NewtonsoftJson

--- SocialMedia.Api/Startup.cs
+++ SocialMedia.Api/Startup.cs
@@ -22,7 +22,10 @@ namespace SocialMedia.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
 
             services.AddDbContext<SocialMediaContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("SocialMedia"))
