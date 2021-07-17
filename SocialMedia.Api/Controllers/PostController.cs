08d62317514707a82135f7bb030f7de103661085
gubiarpa <billy.arredondo@pucp.pe>
2021-07-17 18:40:54 -0500

    post dto added

--- SocialMedia.Api/Controllers/PostController.cs
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Core.DTOs;
 using SocialMedia.Core.Entities;
 using SocialMedia.Core.Interfaces;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SocialMedia.Api.Controllers
@@ -20,18 +22,42 @@ namespace SocialMedia.Api.Controllers
         public async Task<IActionResult> GetPosts()
         {
             var posts = await _postRepository.GetPosts();
-            return Ok(posts);
+            var postsDto = posts.Select(x => new PostDto()
+            {
+                PostId = x.PostId,
+                PostDate = x.PostDate,
+                Description = x.Description,
+                Image = x.Image,
+                UserId = x.UserId
+            });
+            return Ok(postsDto);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetPost(int id)
         {
             var post = await _postRepository.GetPost(id);
-            return Ok(post);
+            var postDto = new PostDto()
+            {
+                PostId = post.PostId,
+                PostDate = post.PostDate,
+                Description = post.Description,
+                Image = post.Image,
+                UserId = post.UserId
+            };
+            return Ok(postDto);
         }
 
-        public async Task<IActionResult> Post(Post post)
+        public async Task<IActionResult> Post(PostDto postDto)
         {
+            var post = new Post()
+            {
+                PostId = postDto.PostId,
+                PostDate = postDto.PostDate,
+                Description = postDto.Description,
+                Image = postDto.Image,
+                UserId = postDto.UserId
+            };
             await _postRepository.InsertPost(post);
             return Ok(post);
         }
