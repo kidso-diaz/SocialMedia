5c1351af9f51158d6ba5139dc787a38680735e92
gubiarpa <billy.arredondo@pucp.pe>
2021-07-17 21:26:54 -0500

    cleaning usings

--- SocialMedia.Api/Controllers/PostController.cs
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -4,7 +4,6 @@ using SocialMedia.Core.DTOs;
 using SocialMedia.Core.Entities;
 using SocialMedia.Core.Interfaces;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace SocialMedia.Api.Controllers
--- SocialMedia.Infrastructure/Data/Configurations/UserConfiguration.cs
+++ SocialMedia.Infrastructure/Data/Configurations/UserConfiguration.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using SocialMedia.Core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SocialMedia.Infrastructure.Data.Configurations
 {
