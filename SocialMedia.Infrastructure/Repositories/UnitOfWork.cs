2cfed1e8251a9aef526107262293ef73033cc855
gubiarpa <billy.arredondo@pucp.pe>
2021-07-26 07:48:19 -0500

    cleaning usings

--- SocialMedia.Infrastructure/Repositories/UnitOfWork.cs
+++ SocialMedia.Infrastructure/Repositories/UnitOfWork.cs
@@ -1,9 +1,6 @@
 ﻿using SocialMedia.Core.Entities;
 using SocialMedia.Core.Interfaces;
 using SocialMedia.Infrastructure.Data;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SocialMedia.Infrastructure.Repositories
